b2937b530100606f21588087a90e33194b4d165c
Daniel Schmidt <daniel.adam.schmidt@gmail.com>
2015-10-10 02:09:48 -0500

    generate routes for orgs and their repos

--- app/router.js
+++ app/router.js
@@ -2,10 +2,21 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
+    this.route('orgs', {});
+    this.route('org', {path: 'org/:org_id'}, function() {
+        this.route('repos', {});
+
+        this.route('repo', {
+            path: ':repo_id'
+        }, function() {
+            this.route('issues', {});
+            this.route('contributors', {});
+        });
+    });
 });
 
 export default Router;
