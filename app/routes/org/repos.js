d87a92a417ee8a5a8327537bb34878b455f45a60
Daniel Schmidt <daniel.adam.schmidt@gmail.com>
2015-10-16 02:55:17 -0500

    exercise 2 complete

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+        {repo_id: 'react'},
+        {repo_id: 'react-native'},
+        {repo_id: 'watchman'}
+    ]
+  }
 });
